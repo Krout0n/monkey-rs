fe340586c65657f8f8347b22fe263bfdc2997bd9
Knium <hi.im.knium@gmail.com>
2018-06-27 22:31:10 +0900

    簡単な構文での字句解析ができるようになった

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![feature(exclusive_range_pattern)]
+#![feature(if_while_or_patterns)]
+
 pub mod token {
 
     #[derive(Debug, PartialEq)]
@@ -15,12 +18,26 @@ pub mod token {
 
         Semicolon,
         Comma,
+
+        Let,
+        Function,
+        Ident(String),
+        Int(i32),
+    }
+
+    pub fn lookup_keyword(literal: String) -> Token {
+        match &*literal {
+            "let" => Token::Let,
+            "fn" => Token::Function,
+            _ => Token::Ident(literal),
+        }
     }
 }
 
 pub mod lexer {
     use token::*;
 
+    #[derive(Debug)]
     pub struct Lexer {
         src: String,
         position: usize,
@@ -42,6 +59,7 @@ pub mod lexer {
 
         pub fn next_token(&mut self) -> Token {
             self.read_char();
+            self.skip_whitespace();
 
             match self.ch {
                 Some('=') => Token::Assign,
@@ -51,8 +69,35 @@ pub mod lexer {
                 Some('}') => Token::RBrace,
                 Some('(') => Token::LParen,
                 Some(')') => Token::RParen,
+                Some(',') => Token::Comma,
                 None => Token::EOF,
-                Some(c) => Token::Illegal(c)
+                Some(c) => match c {
+                    'a'..'z' | 'A'..'Z' | '_' => {
+                        let mut literal = String::new();
+                        while let Some('a'..'z') | Some('A'..'Z') | Some('_') = self.ch {
+                            literal.push_str(&self.ch.unwrap().to_string());
+                            self.read_char();
+                        }
+                        self.backtick();
+                        lookup_keyword(literal)
+                    }
+                    '0'..'9' => {
+                        let mut literal = String::new();
+                        while let Some('0'..'9') = self.ch {
+                            literal.push_str(&self.ch.unwrap().to_string());
+                            self.read_char();
+                        }
+                        self.backtick();
+                        Token::Int(literal.parse::<i32>().unwrap())
+                    }
+                    _ => Token::Illegal(c),
+                },
+            }
+        }
+
+        fn skip_whitespace(&mut self) {
+            while let Some('\t') | Some(' ') | Some('\n') | Some('\r') = self.ch {
+                self.read_char();
             }
         }
 
@@ -65,14 +110,20 @@ pub mod lexer {
                 self.read_position += 1;
             }
         }
+
+        fn backtick(&mut self) {
+            self.read_position -= 1;
+            self.position = self.read_position - 1;
+            self.ch = self.src.chars().nth(self.position);
+        }
     }
 
 }
 
 mod tests {
     use super::*;
-    use token::*;
     use lexer::*;
+    use token::*;
 
     #[test]
     fn some_operand() {
@@ -90,6 +141,63 @@ mod tests {
         let mut l = Lexer::new(input);
         for t in expected {
             assert_eq!(l.next_token(), t);
+        }
+    }
+
+    #[test]
+    fn let_ident() {
+        let input = "\
+                     let five = 5;\n\
+                     let ten = 10;\n\
+                     let add = fn (x ,y) {\n\
+                     x + y;\n\
+                     };\n\
+                     let result = add(five, ten);"
+            .to_string();
+
+        let expected = vec![
+            Token::Let,
+            Token::Ident("five".to_string()),
+            Token::Assign,
+            Token::Int(5),
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident("ten".to_string()),
+            Token::Assign,
+            Token::Int(10),
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident("add".to_string()),
+            Token::Assign,
+            Token::Function,
+            Token::LParen,
+            Token::Ident('x'.to_string()),
+            Token::Comma,
+            Token::Ident('y'.to_string()),
+            Token::RParen,
+            Token::LBrace,
+            Token::Ident('x'.to_string()),
+            Token::Plus,
+            Token::Ident('y'.to_string()),
+            Token::Semicolon,
+            Token::RBrace,
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident("result".to_string()),
+            Token::Assign,
+            Token::Ident("add".to_string()),
+            Token::LParen,
+            Token::Ident("five".to_string()),
+            Token::Comma,
+            Token::Ident("ten".to_string()),
+            Token::RParen,
+            Token::Semicolon,
+        ];
+
+        let mut l = Lexer::new(input);
+        for (i, t) in expected.into_iter().enumerate() {
+            let result = l.next_token();
+            assert_eq!(result, t);
         }
     }
 }
